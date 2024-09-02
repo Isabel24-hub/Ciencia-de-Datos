e7424660e695f295d1cbcb953dd81ffd2007bdef
Isabel24-hub <l20140916@queretaro.tecnm.mx>
2024-09-02 16:29:18 -0600

    Delete programas/programas directory

--- programas/programas/main.py
+++
@@ -1,4 +0,0 @@
-import utils.imports as imp
-
-data = imp.read_diabetes("data/diabetes.tab.txt")
-print(data)
--- programas/programas/utils/imports.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-def read_diabetes(path):
-    return pd.read_csv(path, delimiter='\t')
